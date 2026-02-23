e859967abbd7b16a237008fbad7b50bd104e2a44
Rakesh-Kumar0 <rakeshk49312@gmail.com>
2026-02-23 22:29:04 +0530

    adding new file

---
+++ I2.java
@@ -0,0 +1,31 @@
+interface I1 {
+   
+    int a = 10;
+    void show();
+    void dispaly();
+}
+interface I2{
+    void output();
+}
+
+class B1 implements I1,I2 {
+    public void show(){
+        System.out.println("Methos show");
+    }
+    public void dispaly(){
+        //a = a + 1;
+        System.out.println("Method dispaly " + a);
+    }
+
+     public void output(){
+        
+        System.out.println("Method output " );
+     }
+    public static void main(String[] args) {
+        B1 b = new B1();
+        b.show();
+        b.dispaly();
+        b.output();
+    }
+}
+
