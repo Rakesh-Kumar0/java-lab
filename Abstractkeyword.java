a6a48d80e20cdab777699917aedb56529195fd38
Rakesh-Kumar0 <rakeshk49312@gmail.com>
2026-03-02 21:41:50 +0530

    Adding new file

---
+++ Abstractkeyword.java
@@ -0,0 +1,21 @@
+abstract class Car{
+    public abstract void drive();
+
+    
+    public void playMusic(){
+        System.out.println("play music");
+    }
+}
+class WagonR extends Car{
+    public void drive(){
+        System.out.println("Driving..");
+    }
+}
+public class Abstractkeyword {
+    public static void main(String [] args){
+        Car obj = new WagonR();
+        obj.drive();
+        obj.playMusic();
+    }
+    
+}
