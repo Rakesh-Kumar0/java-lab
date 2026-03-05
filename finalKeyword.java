33538830f60104c9ec555423b591a7a78d5ec9ff
Rakesh-Kumar0 <rakeshk49312@gmail.com>
2026-03-05 22:58:50 +0530

    adding new file

---
+++ finalKeyword.java
@@ -0,0 +1,9 @@
+// final -> variable, method, class
+public class finalKeyword 
+{ public static void main(String [] args){
+    final int num = 8; // use to make a constant variable
+    
+    System.out.println(num);
+}
+    
+}
