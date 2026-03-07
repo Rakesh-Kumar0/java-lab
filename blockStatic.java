40d1b21cc3c10dbc443e2371f41867fd8aa39c78
Rakesh-Kumar0 <rakeshk49312@gmail.com>
2026-03-07 23:26:11 +0530

    adding new file

---
+++ blockStatic.java
@@ -0,0 +1,12 @@
+public class blockStatic {
+    
+
+    static {
+        System.out.println("Static block executed");
+    }
+
+    public static void main(String args[]){
+        System.out.println("Main method executed");
+    }
+}
+
