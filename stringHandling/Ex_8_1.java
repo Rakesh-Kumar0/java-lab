2d2935dfaa5387fb9522e3d28f3bc064fa89ed86
Rakesh-Kumar0 <rakeshk49312@gmail.com>
2026-05-04 23:12:02 +0530

    adding new file

---
+++ stringHandling/Ex_8_1.java
@@ -0,0 +1,11 @@
+//1. Using String Literal
+
+class Ex_8_1{
+    public static void main(String args[]){
+        System.out.println("Rakesh Kumar");
+        String s1="Welcome";
+        String s2="Welcome";
+        System.out.println(s1==s2);
+        System.out.println(s1.equals(s2));
+    }
+}
