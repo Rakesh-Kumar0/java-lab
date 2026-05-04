97bde9919443f1c756f905c57ab85f9d8ea21b39
Rakesh-Kumar0 <rakeshk49312@gmail.com>
2026-05-04 23:13:29 +0530

    adding new file

---
+++ stringHandling/Ex_8_2.java
@@ -0,0 +1,10 @@
+//2. Using new Keyword
+class Ex_8_2{
+    public static void main(String args[]){
+        System.out.println("Rakesh Kumar");
+        String s1=new String("Welcome");
+        String s2=new String("Welcome");
+        System.out.println(s1==s2);
+        System.out.println(s1.equals(s2));
+    }
+}
