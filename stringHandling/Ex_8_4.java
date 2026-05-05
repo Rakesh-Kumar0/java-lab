d10f649307bf02f235a92b6f2a61863f4fdaa121
Rakesh-Kumar0 <rakeshk49312@gmail.com>
2026-05-05 23:30:57 +0530

    adding new file

---
+++ stringHandling/Ex_8_4.java
@@ -0,0 +1,8 @@
+//Immutable String using String Literal
+class Ex_8_4{
+    public static void main(String args[]){
+        String a = "Hello"; //String Literal is immutable
+        a.concat("Java");
+        System.out.println("After Concat: "+a);
+        }
+}
