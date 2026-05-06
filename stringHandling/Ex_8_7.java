72a8af52e9492d4885420e7f55c0f379aa726749
Rakesh-Kumar0 <rakeshk49312@gmail.com>
2026-05-06 23:35:59 +0530

    adding new file

---
+++ stringHandling/Ex_8_7.java
@@ -0,0 +1,10 @@
+
+//Mutable String in Java using StringBuilder class
+class Ex_8_7{
+    public static void main(String args[]){
+        System.out.println("Rakesh Kumar");
+        StringBuilder sb1=new StringBuilder("Hello ");
+        sb1.append("Java");
+        System.out.println("After Concat:"+sb1);
+    }
+}
