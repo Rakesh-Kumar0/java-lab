708f9a487e4a6117919526e9f9871c3eaa8b6374
Rakesh-Kumar0 <rakeshk49312@gmail.com>
2026-05-06 23:35:06 +0530

    adding new file

---
+++ stringHandling/Ex_8_5.java
@@ -0,0 +1,9 @@
+// Immutable String using String class
+class Ex_8_5{
+    public static void main(String args[]){
+        System.out.println("Rakesh Kumar");
+        String a=new String("Hello");
+        a.concat("Java");
+        System.out.println("After Concat: "+a);
+    }
+}
