89e7a89b91030dcc58e8a35310f868d59da86b9f
Rakesh-Kumar0 <rakeshk49312@gmail.com>
2026-05-06 23:35:37 +0530

    adding new file

---
+++ stringHandling/Ex_8_6.java
@@ -0,0 +1,10 @@
+
+//Mutable String in Java using StringBuffer class
+class Ex_8_6{
+    public static void main(String args[]){
+        System.out.println("Rakesh Kumar");
+        StringBuffer sb=new StringBuffer("Hello ");
+        sb.append("Java");
+        System.out.println("After Concat:"+sb);
+    }
+}
