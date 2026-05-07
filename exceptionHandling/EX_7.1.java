38ad88f96f064c0ce8ea1f668627c395851a4a5a
Rakesh-Kumar0 <rakeshk49312@gmail.com>
2026-05-07 21:49:40 +0530

    adding new file

---
+++ exceptionHandling/EX_7.1.java
@@ -0,0 +1,12 @@
+
+//without exception handling
+class EX_7_1{
+    public static void main(String args[]){
+        System.out.println("Rakesh Kumar");
+        System.out.println("1");
+        int a=1/0;
+        System.out.println("2");
+        System.out.println("3");
+    }
+}
+
