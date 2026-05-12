9f5c67c95927ff86078d680762a373a6eeb973ec
Rakesh-Kumar0 <rakeshk49312@gmail.com>
2026-05-12 23:30:58 +0530

    adding new file

---
+++ swing/GridLayoutDemo.java
@@ -0,0 +1,25 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class GridLayoutDemo {
+
+    public static void main(String[] args) {
+
+        JFrame frame =
+            new JFrame("GridLayout Example");
+
+        frame.setLayout(new GridLayout(2, 2));
+
+        frame.add(new JButton("Button 1"));
+        frame.add(new JButton("Button 2"));
+        frame.add(new JButton("Button 3"));
+        frame.add(new JButton("Button 4"));
+
+        frame.setSize(300, 200);
+
+        frame.setDefaultCloseOperation(
+            JFrame.EXIT_ON_CLOSE);
+
+        frame.setVisible(true);
+    }
+}
