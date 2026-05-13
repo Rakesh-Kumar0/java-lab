259d8b7381896b56b41177397db2a38cbff3aafd
Rakesh-Kumar0 <rakeshk49312@gmail.com>
2026-05-13 21:03:19 +0530

    adding new file

---
+++ threadandcollection/MapExample.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class MapExample {
+    public static void main(String[] args) {
+        System.out.println("Rakesh Kumar");
+        HashMap<Integer, String> map = new HashMap<>();
+        map.put(1, "Rakesh");
+        map.put(2, "Kumar");
+
+        System.out.println(map);
+    }
+}
