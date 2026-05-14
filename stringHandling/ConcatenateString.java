3543d67a98b9639b4bea76ceeffcc81630d3a33b
Rakesh-Kumar0 <rakeshk49312@gmail.com>
2026-05-14 23:13:33 +0530

    adding new file

---
+++ stringHandling/ConcatenateString.java
@@ -0,0 +1,13 @@
+public class ConcatenateString {
+    public static void main(String[] args) {
+        String first = "Rakesh";
+        String last = "Kumar";
+
+        // Using concat()
+        System.out.println(first.concat(" " + last));
+
+        // Using + operator
+        System.out.println(first + " " + last);
+    }
+}
+
