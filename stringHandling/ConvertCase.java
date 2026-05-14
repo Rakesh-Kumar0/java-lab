d87c052a08e64105ac6d6d1aa2c0c54ed220afd1
Rakesh-Kumar0 <rakeshk49312@gmail.com>
2026-05-14 23:14:10 +0530

    adding new file

---
+++ stringHandling/ConvertCase.java
@@ -0,0 +1,7 @@
+public class ConvertCase {
+    public static void main(String[] args) {
+        String name = "Rakesh Kumar";
+        System.out.println("Uppercase: " + name.toUpperCase());
+        System.out.println("Lowercase: " + name.toLowerCase());
+    }
+}
