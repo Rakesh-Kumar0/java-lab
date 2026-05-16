0e8179d1ccae96cac7c216fbaaee04cd0da410c0
Rakesh-Kumar0 <rakeshk49312@gmail.com>
2026-05-16 22:36:28 +0530

    adding new file

---
+++ stringHandling/FindCharacter.java
@@ -0,0 +1,9 @@
+public class FindCharacter {
+    public static void main(String[] args) {
+        String name = "Rakesh Kumar";
+        int index = 2;
+
+        System.out.println("Character at index " + index + ": " + name.charAt(index));
+    }
+}
+
